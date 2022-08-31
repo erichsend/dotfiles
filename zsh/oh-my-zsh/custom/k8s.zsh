f9fa0b11618d926c9b966a6c56afdaf075c9c952
David Erichsen <david.erichsen@konghq.com>
2022-08-31 07:04:46 -0500

    feat: add helm-fan-out function - This function will render a helm chart into one k8s manifest per   template. Useful for quick iterations when building a new template in   a larger chart and applying the new single resource after rendering.

--- zsh/oh-my-zsh/custom/k8s.zsh
+++ zsh/oh-my-zsh/custom/k8s.zsh
@@ -5,3 +5,31 @@ alias kfull='kubectl config get-contexts'
 alias ktx='kubectx'
 alias kns='kubens'
 function k() { kubectl "$@"; }
+
+# helm-fan-out: render each template into its own manifest which can be helpful
+# for only applying a single resource during testing/debugging
+# Based on https://github.com/helm/helm/issues/4680#issuecomment-613201032
+function helm-fan-out() { 
+if [ -z "$1" ]; then
+    echo "Please provide an output directory"
+    exit 1
+fi
+
+awk -vout="$1" -F": " '
+  $0~/^# Source: / {
+    file=out"/"$2;
+    if (!(file in filemap)) {
+      filemap[file] = 1
+      print "Creating "file;
+      system ("mkdir -p $(dirname "file")");
+      print "---" >> file;
+    }
+  }
+  $0!~/^# Source: / {
+    if ($0!~/^---$/) {
+      if (file) {
+        print $0 >> file;
+      }
+    }
+  }'
+}
