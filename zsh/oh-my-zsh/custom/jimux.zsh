c029e59ef33c34d9f3f792f5f46c885c6a915719
David Erichsen <david.erichsen@konghq.com>
2022-11-10 22:24:52 -0600

    adding colors, history

--- zsh/oh-my-zsh/custom/jimux.zsh
+++ zsh/oh-my-zsh/custom/jimux.zsh
@@ -2,79 +2,6 @@
 # Is it bad? Yes. Does it work? Yes.
 LIST_FILE="$HOME/.config/.jira/.jimux.tmp"
 
-# Reset
-Color_Off='\033[0m'       # Text Reset
-
-# Regular Colors
-Black='\033[0;30m'        # Black
-Red='\033[0;31m'          # Red
-Green='\033[0;32m'        # Green
-Yellow='\033[0;33m'       # Yellow
-Blue='\033[0;34m'         # Blue
-Purple='\033[0;35m'       # Purple
-Cyan='\033[0;36m'         # Cyan
-White='\033[0;37m'        # White
-
-# Bold
-BBlack='\033[1;30m'       # Black
-BRed='\033[1;31m'         # Red
-BGreen='\033[1;32m'       # Green
-BYellow='\033[1;33m'      # Yellow
-BBlue='\033[1;34m'        # Blue
-BPurple='\033[1;35m'      # Purple
-BCyan='\033[1;36m'        # Cyan
-BWhite='\033[1;37m'       # White
-
-# Underline
-UBlack='\033[4;30m'       # Black
-URed='\033[4;31m'         # Red
-UGreen='\033[4;32m'       # Green
-UYellow='\033[4;33m'      # Yellow
-UBlue='\033[4;34m'        # Blue
-UPurple='\033[4;35m'      # Purple
-UCyan='\033[4;36m'        # Cyan
-UWhite='\033[4;37m'       # White
-
-# Background
-On_Black='\033[40m'       # Black
-On_Red='\033[41m'         # Red
-On_Green='\033[42m'       # Green
-On_Yellow='\033[43m'      # Yellow
-On_Blue='\033[44m'        # Blue
-On_Purple='\033[45m'      # Purple
-On_Cyan='\033[46m'        # Cyan
-On_White='\033[47m'       # White
-
-# High Intensity
-IBlack='\033[0;90m'       # Black
-IRed='\033[0;91m'         # Red
-IGreen='\033[0;92m'       # Green
-IYellow='\033[0;93m'      # Yellow
-IBlue='\033[0;94m'        # Blue
-IPurple='\033[0;95m'      # Purple
-ICyan='\033[0;96m'        # Cyan
-IWhite='\033[0;97m'       # White
-
-# Bold High Intensity
-BIBlack='\033[1;90m'      # Black
-BIRed='\033[1;91m'        # Red
-BIGreen='\033[1;92m'      # Green
-BIYellow='\033[1;93m'     # Yellow
-BIBlue='\033[1;94m'       # Blue
-BIPurple='\033[1;95m'     # Purple
-BICyan='\033[1;96m'       # Cyan
-BIWhite='\033[1;97m'      # White
-
-# High Intensity backgrounds
-On_IBlack='\033[0;100m'   # Black
-On_IRed='\033[0;101m'     # Red
-On_IGreen='\033[0;102m'   # Green
-On_IYellow='\033[0;103m'  # Yellow
-On_IBlue='\033[0;104m'    # Blue
-On_IPurple='\033[0;105m'  # Purple
-On_ICyan='\033[0;106m'    # Cyan
-On_IWhite='\033[0;107m'   # White
-
 JIRPL_USAGE=$(cat <<-END
 # Interactive Commands
 (a)ssign    (e)dit    (m)ove
@@ -91,9 +18,11 @@ JIRPL_USAGE=$(cat <<-END
 (P)rioritized
 (D)eprioritized
 
+# Quick Transition
+(C)lose
 
 (x) Next Issue
-(?) Show History (not implemented)
+(?) Search History
 (!) Refresh List (not implemented)
 
 END
@@ -109,8 +38,12 @@ function _getParent() {
     --header 'Accept: application/json' --silent | jq -r '.fields.parent.key'
 }
 
+function _searchHistory() {
+  tmux display-popup "tail -r $LIST_FILE.log | fzf"
+} 
+
 function _loadList() {
-  local _jira_cmd="jira issue list -q '$1' --plain --columns key,summary | tail -n +2"
+  local _jira_cmd="jira issue list -q '$1' --plain --columns key,summary,status,assignee | tail -n +2"
   (eval $_jira_cmd) > $LIST_FILE
 }
 
@@ -119,10 +52,12 @@ function _printStatus() {
   echo "${BIPurple}"
   echo "Tickets Remaining: $(cat $LIST_FILE | wc -l)\n\n"
   echo "${BIBlue}Coming Up...${Blue}"
-  sed -n '2,10p' $LIST_FILE
-  echo "----------------------------------"
+  sed -n '2,10p' $LIST_FILE | cut -f 1-2 | cut -c -100
   echo "${BIGreen}\n\nJiREPL Key Bindings"
-  echo "${Green}\n$JIRPL_USAGE${Color_Off}"
+  echo "${Green}\n$JIRPL_USAGE"
+  echo "\n${Color_Off}"
+  echo "${BICyan}Recent History..."
+  tail -n 10 $LIST_FILE.log | sort -r
 }
 
 function _jiraSearchLoopCache() {
@@ -135,13 +70,14 @@ function _jiraSearchLoopCache() {
   while read -sk && [[ $REPLY != q ]]; do
     case $REPLY in
       s) _printStatus ;;
+      h) _searchHistory ;;
       *) echo "Try again..." ;;
     esac
   done
 }
 
 function jiraIssueBacklogTriage() {
-	_jiraSearchLoopCache "type in standardIssueTypes() AND type != Epic AND statusCategory != Done AND (component not in (billing-provisioning, runtime-manager, admin-cx-tool, service-hub, konnect-backend, konnect-ui, users-teams, kongponents, kong-auth-elements) OR component is EMPTY) AND project = KHCP"
+	_jiraSearchLoopCache "type in standardIssueTypes() AND type != Epic AND statusCategory != Done AND (component not in (open-source,billing-provisioning, runtime-manager, admin-cx-tool, service-hub, konnect-backend, konnect-ui, users-teams, kongponents, kong-auth-elements) OR component is EMPTY) AND project = KHCP"
 }
 
 function jimux() {
@@ -153,27 +89,11 @@ function jimux() {
   jiraIssueBacklogTriage
 }
 
-####### -- This should be changed to probably be some kind of List Reload and restart repl
+#######
 # The section below defines functions meant to be called from 'jrepl'. 
 # Just doing this to keep all of the tmux commands in one file.
 #########
 # Assumes we are already in a side-by-side jimux/jrepl split
-function remoteRefresh() {
-  tmux send -t left "s"
+function remoteKey() {
+  tmux send -t left "$1"
 }
-
-######
-# Evertying below is considered deprecated.
-# Just keeping for reference until refactor is complete.
-# Re-use some of the searches and feed them into the cache list
-######
-
-# Two-step search: find epic and start issue search.
-# Deprecate: Use regular search with type=epic and then in jrepl use 'list children'
-function jiraQuickEpicSearch() {
-  clear
-  _jira_cmd="jira epic list --table --plain --columns key,status,summary | fzf --height 100% --border --color 'border:$_epic_border' | cut -f 1"
- _iss=$(eval $_jira_cmd)
- jiraEpicIssueList $_iss
-}
-
--- zsh/oh-my-zsh/custom/jirepl.zsh
+++ zsh/oh-my-zsh/custom/jirepl.zsh
@@ -6,41 +6,53 @@ function _popList() {
   echo $_result
 }
 
+function _printCurrentTicket () {
+  echo "\n\n${Blue}Current Ticket:\n${BIBlue}$(echo $line | sed -E 's/\t+/  ||  /g')\n\n${Color_Off}"
+}
+
+function _logChange () {
+  echo "$(date +'%m/%d@%H:%M') -- $1" | tee -a ${LIST_FILE}.log
+}
+
 function jirepl() {
   clear
   LIST_FILE=$1
   line=$(_popList)
   key=$(cut -f 1 <<< $line)
-  echo "\n\n${Blue}Current Ticket:\n${BIBlue}$line\n\n${Color_Off}"
+  _printCurrentTicket
   while echo "${BWhite}Choose an action\n${Color_Off}" && read -sk && [[ $REPLY != q ]]; do case $REPLY in
     # Change current issue
-    a) jira issue assign $key;;
-    e) jira issue edit $key;;
-    m) jira issue move $key;;
+    a) _logChange "$key | Assigned" && jira issue assign $key;;
+    e) _logChange "$key | Edited" && jira issue edit $key;;
+    m) _logChange "$key | Moved" && jira issue move $key;;
     o) jira open $key;;
     v) jira issue view $key --plain;;
 
     # Quick-Change Commands
     # Consider making the component add be a function, and having it remove other components.
     # Consider adding to a 'history' array to display on next issue
-    A) echo "Adding Component: admin-cx-tool" && jira issue edit $key -Cadmin-cx-tool --no-input ;;
-    B) echo "Adding Component: billing-provisioning" && jira issue edit $key -Cbilling-provisioning --no-input ;;
-    D) echo "Adding Label: deprioritized" && jira issue edit $key -ldeprioritize --no-input ;;
-    K) echo "Adding Component: konnect-backend" && jira issue edit $key -Ckonnect-backend --no-input ;;
-    N) echo "Adding Label: needs-refinement" && jira issue edit $key -lneeds-refinement --no-input ;;
-    P) echo "Adding Label: prioritized" && jira issue edit $key -lprioritize --no-input ;;
-    R) echo "Adding Component: runtime-manager" && jira issue edit $key -Cruntime-manager --no-input ;;
-    S) echo "Adding Component: service-hub" && jira issue edit $key -Cservice-hub --no-input ;;
-    U) echo "Adding Component: konnect-ui" && jira issue edit $key -Ckonnect-ui --no-input ;;
+    A) _logChange "$key | Adding Component: admin-cx-tool" && jira issue edit $key -Cadmin-cx-tool --no-input ;;
+    B) _logChange "$key | Adding Component: billing-provisioning" && jira issue edit $key -Cbilling-provisioning --no-input ;;
+    C) _logChange "$key | Closing Issue" && jira issue move $key "Resolved" ;;
+    D) _logChange "$key | Adding Label: deprioritized" && jira issue edit $key -ldeprioritize --no-input ;;
+    K) _logChange "$key | Adding Component: konnect-backend" && jira issue edit $key -Ckonnect-backend --no-input ;;
+    N) _logChange "$key | Adding Label: needs-refinement" && jira issue edit $key -lneeds-refinement --no-input ;;
+    P) _logChange "$key | Adding Label: prioritized" && jira issue edit $key -lprioritize --no-input ;;
+    R) _logChange "$key | Adding Component: runtime-manager" && jira issue edit $key -Cruntime-manager --no-input ;;
+    S) _logChange "$key | Adding Component: service-hub" && jira issue edit $key -Cservice-hub --no-input ;;
+    U) _logChange "$key | Adding Component: konnect-ui" && jira issue edit $key -Ckonnect-ui --no-input ;;
 
     # TMUX Commands 
     x) 
       line=$(_popList)
       key=$(cut -f 1 <<< $line)
       clear
-      echo "\n\nCurrent Ticket >>> $line\n"
-      remoteRefresh
+      _printCurrentTicket
+      remoteKey "s"
       ;;
+    ?) 
+      echo "Displaying History...\n\n\n"
+      remoteKey "h" ;;
     *) echo "Try again..." ;;
     esac
   done
