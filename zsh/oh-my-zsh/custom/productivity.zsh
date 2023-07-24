8cc6d3dc78c55fa1f6e3a4baa9a5964c4f53cf5a
David Erichsen <david.erichsen@konghq.com>
2023-07-23 21:21:19 -0500

    chore: updated tmuxp environments and aliases, added gpt alias, added volta stuff

--- zsh/oh-my-zsh/custom/productivity.zsh
+++ zsh/oh-my-zsh/custom/productivity.zsh
@@ -11,6 +11,17 @@ alias zd='cd ~/.oh-my-zsh/custom'
 alias td='cd ~/.tmux'
 alias dd='cd ~/repo/dotfiles/'
 
-
 # alias to send output to vim
 alias v='nvim -'
+
+# quick gpt
+function gpt() { echo "$@" | openai complete - --token $OPENAI_API_KEY }
+
+# Tools Environment
+alias tools="tmuxp load ~/.config/tmuxp/tools.yaml -d"
+
+# Dotfiles Environment
+alias dotfiles="tmuxp load ~/.config/tmuxp/dotfiles.yaml -d"
+
+# Dev Environment
+function dev () { REPO="$@" && tmuxp load ~/.config/tmuxp/dev.yaml -d }
