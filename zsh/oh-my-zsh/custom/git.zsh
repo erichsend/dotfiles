23582264d70457c67e873b1fa4fa4b58d59967f0
David Erichsen <david.erichsen@konghq.com>
2024-01-17 19:14:27 -0600

    add ghc for github clone and khck for github clone of kong repos

--- zsh/oh-my-zsh/custom/git.zsh
+++ zsh/oh-my-zsh/custom/git.zsh
@@ -1,4 +1,5 @@
 alias glog="git --no-pager log --oneline -n 10"
 
-alias ghc='function _gclone(){ git clone git@github.com:$1/$2.git; }; _gclone'
+alias ghc='function _ghc(){ git clone git@github.com:$1.git; }; _ghc'
+alias ghck='function _ghck(){ git clone git@github.com:kong/$1.git; }; _ghck'
 
