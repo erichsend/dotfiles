10a6707afd6cac8ee62a94e56b3fc826c288e094
David Erichsen <david.erichsen@konghq.com>
2024-02-10 23:00:31 -0600

    feat: add asdf setup

---
+++ zsh/oh-my-zsh/custom/asdf.zsh
@@ -0,0 +1,9 @@
+ASDF_DIR="$HOME/.asdf"
+
+if [ -f ${ASDF_DIR} ]
+  . "${ASDF_DIR}/asdf.sh"
+  # append completions to fpath
+  fpath=(${ASDF_DIR}/completions $fpath)
+  # initialise completions with ZSH's compinit
+  autoload -Uz compinit && compinit
+fi
